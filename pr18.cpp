1346586f31bf3176153185ad1642e5b52219e2c6
Sasmitha Kariyawasam <Sasamavikariyawasam12@gmail.com>
2024-11-17 20:55:46 +0530

    Add files via upload

---
+++ pr18.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int x;
+    cout<<"Enter a value: ";
+    cin>>x;
+
+    cout<<"Post decrement"<<endl;
+
+    cout<<"First use of x is: "<<x<<endl;
+
+    x--;
+
+    cout<<"Second use of x is: "<<x<<endl;
+
+    cout<<"Pre decrement"<<endl;
+
+    cout<<"First use of x is: "<<x<<endl;
+
+    --x;
+
+    cout<<"Second use of x is: "<<x;
+
+
+}
+
